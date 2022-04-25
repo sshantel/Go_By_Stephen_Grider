2748ad07a5efb27e424a2fa4d4d792995deb214f
Chantel Yip <cyip@jackhenry.com>
2022-04-25 19:13:32 -0400

    turn Deck into a string

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
+import "fmt"
+
 func main() {
+	// cards := newDeck()
+	// hand, remainingCards := deal(cards, 5)
+	// hand.print()
+	// remainingCards.print()
 	cards := newDeck()
-	hand, remainingCards := deal(cards, 5)
-	hand.print()
-	remainingCards.print()
+	fmt.Println(cards.toString())
 }
