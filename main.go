e6ae9dd5c1bffc2c0d911ed4af8dece6cfacc713
Chantel Yip <cyip@jackhenry.com>
2022-04-25 08:12:04 -0400

    add deck as a receiver

--- main.go
+++ main.go
@@ -1,15 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
 	// var card string = "Ace of Spades"
 	cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards, "Six of Spades")
-	fmt.Println(cards)
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 }
 
 func newCard() string {
