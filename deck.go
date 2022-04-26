86abece85f6ba56cc453d2c9a6ab49874d3ff9fa
Chantel Yip <cyip@jackhenry.com>
2022-04-25 20:01:35 -0400

    save data to hard drive

--- deck.go
+++ deck.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"strings"
 )
 
@@ -37,3 +38,19 @@ func (d deck) toString() string {
 	return strings.Join([]string(d), ",")
 
 }
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+// func newDeckFromFile(filename string) deck {
+// 	bs, err := ioutil.ReadFile(filename)
+// 	if err != nil {
+// 		fmt.Println("Error", err)
+// 		os.Exit(1)
+// 	}
+
+// 	s := strings.Split(string(bs), ",")
+// 	deck(s)
+
+// }
--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
 	// cards := newDeck()
 	// hand, remainingCards := deal(cards, 5)
 	// hand.print()
 	// remainingCards.print()
 	cards := newDeck()
-	fmt.Println(cards.toString())
+	cards.saveToFile("my_cards")
 }
