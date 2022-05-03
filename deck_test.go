80130c793bad4b6ae63bc0953f84759942654978
Chantel Yip <cyip@jackhenry.com>
2022-05-02 21:30:01 -0400

    testing file IO

--- deck_test.go
+++ deck_test.go
@@ -1,10 +1,27 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
 	if len(d) != 16 {
 		t.Errorf("Expected deck length of 20, but got %v", len(d))
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck, got %v", len(loadedDeck))
+	}
+	os.Remove("_decktesting")
+
 }
